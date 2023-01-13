10b7a52b7a6ebc7398d9af9f7e58e73b175e9229
HyunCafe <eparksuncafe@gmail.com>
2023-01-12 19:49:03 -0600

    Format: Formatted with Prettier

--- script2.js
+++ script2.js
@@ -1,4 +1,4 @@
-    /* a game of RPS
+/* a game of RPS
     player can click on 1 of 3 choices R P S
     Computer chooses 1 of 3 choices at random R P S
     Values of player choice and computer choice will be compared with pre determined values of strength
@@ -6,52 +6,51 @@
     There will be a counter to keep tracks of wins and losses
     */
 
-    const gameChoices = ['rock', 'paper', 'scissors'];
-            function getComputerChoice() {
-            return gameChoices[Math.floor(Math.random() * gameChoices.length)];
-            }
-            
-    function playGame () {
+const gameChoices = ["rock", "paper", "scissors"];
+function getComputerChoice() {
+  return gameChoices[Math.floor(Math.random() * gameChoices.length)];
+}
 
-        const rockButton = document.getElementById("rock");
-        const paperButton = document.getElementById("paper");
-        const scissorsButton = document.getElementById("scissors");
+function playGame() {
+  const rockButton = document.getElementById("rock");
+  const paperButton = document.getElementById("paper");
+  const scissorsButton = document.getElementById("scissors");
 
-        rockButton.addEventListener("click", () => {
-            compareSelection('rock');
-        });
-        paperButton.addEventListener("click", () => {
-            compareSelection('paper');
-        });
-        scissorsButton.addEventListener("click", () => {
-            compareSelection('scissors');
-        });
+  rockButton.addEventListener("click", () => {
+    compareSelection("rock");
+  });
+  paperButton.addEventListener("click", () => {
+    compareSelection("paper");
+  });
+  scissorsButton.addEventListener("click", () => {
+    compareSelection("scissors");
+  });
 
-        function compareSelection(playerChoice) {
-            const computerChoice = getComputerChoice();
+  function compareSelection(playerChoice) {
+    const computerChoice = getComputerChoice();
 
-            if (playerChoice === computerChoice) {
-                displayResult("It's a tie!");
-            } else if (playerChoice === 'rock' && computerChoice === 'paper') {
-                displayResult("You Lose! Paper beats Rock!");
-            } else if (playerChoice === 'rock' && computerChoice === 'scissors') {
-                displayResult("You Win! Rock beats Scissors!");
-            } else if (playerChoice === 'paper' && computerChoice === 'scissors') {
-                displayResult("You Lose! Scissors beats Paper!");
-            } else if (playerChoice === 'paper' && computerChoice === 'rock') {
-                displayResult("You Win! Paper beats Rock!");
-            } else if (playerChoice === 'scissors' && computerChoice === 'rock') {
-                displayResult("You Lose! Rock beats Scissors!");
-            } else if (playerChoice === 'scissors' && computerChoice === 'paper') {
-                displayResult("You Win! Scissors beats Paper!");
-            }
-            
-            function displayResult(result) {
-            const resultElement = document.getElementById("result");
-            resultElement.innerHTML = result;
-            }
-        }
+    if (playerChoice === computerChoice) {
+      displayResult("It's a tie!");
+    } else if (playerChoice === "rock" && computerChoice === "paper") {
+      displayResult("You Lose! Paper beats Rock!");
+    } else if (playerChoice === "rock" && computerChoice === "scissors") {
+      displayResult("You Win! Rock beats Scissors!");
+    } else if (playerChoice === "paper" && computerChoice === "scissors") {
+      displayResult("You Lose! Scissors beats Paper!");
+    } else if (playerChoice === "paper" && computerChoice === "rock") {
+      displayResult("You Win! Paper beats Rock!");
+    } else if (playerChoice === "scissors" && computerChoice === "rock") {
+      displayResult("You Lose! Rock beats Scissors!");
+    } else if (playerChoice === "scissors" && computerChoice === "paper") {
+      displayResult("You Win! Scissors beats Paper!");
     }
-    window.onload = function(){
-        playGame();
+
+    function displayResult(result) {
+      const resultElement = document.getElementById("result");
+      resultElement.innerHTML = result;
     }
+  }
+}
+window.onload = function () {
+  playGame();
+};
