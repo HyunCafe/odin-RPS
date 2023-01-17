239f61c12d8ba77a7fb0e30c6c2c3890c8204ab4
HyunCafe <eparksuncafe@gmail.com>
2023-01-16 20:18:12 -0600

    Add: 3rd script version of RPS

---
+++ script3.js
@@ -0,0 +1,40 @@
+//  Computer can choose randomly from an array of game choice for rock paper or scissors
+// play game function which compares choices made for player and computer
+// based on outcome a win or loss display will result
+// Counter for keeping track of winnings that loops and resets at 10 games ultimately displaying final score
+
+const elements = document.querySelectorAll("#rock, #paper, #scissors");
+elements.forEach(function(element) {
+    element.addEventListener("click", playGame);
+});
+
+function displayResult(result) {
+    const resultElement = document.getElementById("result");
+    resultElement.innerHTML = result;
+    }
+
+const choices = ['rock', 'paper', 'scissors'];
+const computerChoice = choices[Math.floor(Math.random()*choices.length)];
+const playerChoice = elements
+const rules = {
+    rock: { beats: "scissors", losesTo: "paper" },
+    paper: { beats: "rock", losesTo: "scissors" },
+    scissors: { beats: "paper", losesTo: "rock" }
+};
+
+function checkWinner(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        return "It's a Tie!";
+    } else if (playerChoice > computerChoice) {
+        return "You Won!";
+    } else {
+        return "You Lost!";
+    }
+}
+
+function playGame() {
+    let playerChoice = this.id;
+    let computerChoice = choices[Math.floor(Math.random()*choices.length)];
+    let result = checkWinner(playerChoice, computerChoice);
+    displayResult(result);
+}
