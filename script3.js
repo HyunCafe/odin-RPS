66da2455f411974f598179f9833ecd3d3a7f248a
HyunCafe <eparksuncafe@gmail.com>
2023-01-29 14:57:56 -0600

    Fix: Code wasn't correctly applying logic for rules

--- script3.js
+++ script3.js
@@ -4,37 +4,36 @@
 // Counter for keeping track of winnings that loops and resets at 10 games ultimately displaying final score
 
 const elements = document.querySelectorAll("#rock, #paper, #scissors");
-elements.forEach(function(element) {
-    element.addEventListener("click", playGame);
+elements.forEach(function (element) {
+  element.addEventListener("click", playGame);
 });
 
 function displayResult(result) {
-    const resultElement = document.getElementById("result");
-    resultElement.innerHTML = result;
-    }
+  const resultElement = document.getElementById("result");
+  resultElement.innerHTML = result;
+}
 
-const choices = ['#rock', '#paper', '#scissors'];
-const computerChoice = choices[Math.floor(Math.random()*choices.length)];
-const playerChoice = elements
+const choices = ["rock", "paper", "scissors"];
+
 const rules = {
-    rock: { beats: "scissors", losesTo: "paper" },
-    paper: { beats: "rock", losesTo: "scissors" },
-    scissors: { beats: "paper", losesTo: "rock" }
+  rock: { beats: "scissors", losesTo: "paper" },
+  paper: { beats: "rock", losesTo: "scissors" },
+  scissors: { beats: "paper", losesTo: "rock" },
 };
 
 function checkWinner(playerChoice, computerChoice) {
-    if (playerChoice === computerChoice) {
-        return "It's a Tie!";
-    } else if (playerChoice > computerChoice) {
-        return "You Won!";
-    } else {
-        return "You Lost!";
-    }
+  if (rules[playerChoice]["beats"] === computerChoice) {
+    return "You Won!";
+  } else if (rules[computerChoice]["beats"] === playerChoice) {
+    return "You Lost!";
+  } else {
+    return "It's a Tie!";
+  }
 }
 
 function playGame() {
-    let playerChoice = this.id;
-    let computerChoice = choices[Math.floor(Math.random()*choices.length)];
-    let result = checkWinner(playerChoice, computerChoice);
-    displayResult(result);
+  let playerChoice = this.id;
+  let computerChoice = choices[Math.floor(Math.random() * choices.length)];
+  let result = checkWinner(playerChoice, computerChoice);
+  displayResult(result);
 }
